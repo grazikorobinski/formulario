6706eed147da8fc981620a02199f9eaa36a2b229
grazikorobinski <korobinski.grazielly@escola.pr.gov.br>
2025-09-15 11:28:53 -0300

    .

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+function mostraPalavrasChave() {
+    const texto = document.querySelector("#entrada-de-texto").value;
+    const campoResultado = document.querySelector("#resultado-palavrachave");
+    const palavrasChave = processaTexto(texto);
 
+    campoResultado.textContent = palavrasChave.join(", ");
+}
 function mostraPalavrasChave() { 
 let palavras = texto.split(/\P{L}+/u);
 
