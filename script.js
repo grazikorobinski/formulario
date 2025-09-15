243180fc8aad5da99ce5cbebe982d64e98b87d63
grazikorobinski <korobinski.grazielly@escola.pr.gov.br>
2025-09-15 11:24:19 -0300

    .

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ let palavras = texto.split(/\P{L}+/u);
     palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras); 
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    return ordenadas.slice(0, 10);
+}
 } 
